8c9f73ce0fe6ee9688b1647b2f346a54078a753b
Ryo Nakamura <r7kamura@gmail.com>
2022-05-22 08:04:51 +0900

    Show all actions in options page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,36 @@
+import { actions } from "../action";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <header>
+        <nav>
+        <h1>TweetDeck Shortcut Plus</h1>
+        </nav>
+      </header>
+      <main>
+        <section>
+          <table>
+            <tbody>
+              {actions.map((action) => {
+                return (
+                  <tr>
+                    <td>{action.description}</td>
+                    <td>
+                      <input
+                        type="text"
+                        autoComplete="off"
+                        readOnly
+                        value={action.default}
+                      />
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </section>
+      </main>
+    </>
+  );
 }
--- src/options.tsx
+++ src/options.tsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import "./style.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root")!;
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
