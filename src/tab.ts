012a963392effc59e0b0597657af32ad45dc0590
Ryo Nakamura <r7kamura@gmail.com>
2022-05-21 10:04:26 +0900

    Refactor: add tab.ts

---
+++ src/tab.ts
@@ -0,0 +1,27 @@
+export function openForegroundTab(url: string) {
+  window?.open(url, "_blank")?.focus();
+}
+
+export function openBackgroundTab(url: string) {
+  var a = document.createElement("a");
+  a.href = url;
+  var event = document.createEvent("MouseEvents");
+  event.initMouseEvent(
+    "click",
+    true,
+    true,
+    window,
+    0,
+    0,
+    0,
+    0,
+    0,
+    true,
+    false,
+    false,
+    false,
+    0,
+    null
+  );
+  a.dispatchEvent(event);
+}
--- src/tweetdeck.ts
+++ src/tweetdeck.ts
@@ -1,4 +1,5 @@
 import { copy } from "./clipboard";
+import { openBackgroundTab, openForegroundTab } from "./tab";
 
 export function addSelectedActivityOperatorColumn() {
   findSelectedActivityOperatorLink()?.click();
@@ -215,32 +216,4 @@ function getUrlOfSelectedTweetAuthor() {
   return document
     .querySelector(".is-selected-tweet a[rel='user']")
     ?.getAttribute("href");
-}
-
-function openForegroundTab(url: string) {
-  window?.open(url, "_blank")?.focus();
-}
-
-function openBackgroundTab(url: string) {
-  var a = document.createElement("a");
-  a.href = url;
-  var event = document.createEvent("MouseEvents");
-  event.initMouseEvent(
-    "click",
-    true,
-    true,
-    window,
-    0,
-    0,
-    0,
-    0,
-    0,
-    true,
-    false,
-    false,
-    false,
-    0,
-    null
-  );
-  a.dispatchEvent(event);
 }
