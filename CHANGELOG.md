c25369f881a7d4352e994329f9ae4ae43c274cb8
Ryo Nakamura <r7kamura@gmail.com>
2022-05-23 12:56:13 +0900

    Revert prevent default

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,7 +9,6 @@
 ### Fixed
 
 - Don't execute keyboard shortcut when `input` or `textarea` is focused.
-- Don't prevent default keydown event.
 
 ## 0.1.0 - 2022-05-23
 
--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,10 @@ chrome.runtime.sendMessage(
       const key = detectKeyString(event)!;
       const commandNames = keyMap[key] || [];
       runCommands(commandNames);
+
+      if (commandNames.length >= 1) {
+        event.preventDefault();
+      }
     });
   }
 );
